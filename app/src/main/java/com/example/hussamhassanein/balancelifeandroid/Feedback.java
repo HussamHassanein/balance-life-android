a7d3411801a9296b230d1d98660bdec8c7afee55
Hussam Hassanein <hussamh@kth.se>
2017-11-23 11:39:19 +0100

    first commit

---
+++ app/src/main/java/com/example/hussamhassanein/balancelifeandroid/Feedback.java
@@ -0,0 +1,17 @@
+package com.example.hussamhassanein.balancelifeandroid;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by HussamHassanein on 2017-11-23.
+ */
+
+public class Feedback extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.feedback);
+    }
+}
