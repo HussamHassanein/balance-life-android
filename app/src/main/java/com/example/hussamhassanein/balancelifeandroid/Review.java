422f7e809dd476bf3f171fdd1d3e6ef8ff4ab5fe
Hussam Hassanein <hussamh@kth.se>
2017-11-24 15:16:40 +0100

    view text in social activity

--- app/src/main/java/com/example/hussamhassanein/balancelifeandroid/Review.java
+++ app/src/main/java/com/example/hussamhassanein/balancelifeandroid/Review.java
@@ -1,13 +1,17 @@
 package com.example.hussamhassanein.balancelifeandroid;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 /**
  * Created by HussamHassanein on 2017-11-23.
  */
 
 public class Review extends AppCompatActivity {
+   // TextView rrr;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,4 +20,11 @@ public class Review extends AppCompatActivity {
         getSupportActionBar().setTitle("Review");
 
     }
+
+
+   /* rrr=(TextView)findViewById(R.id.textView);
+    SharedPreferences share=getSharedPreferences("MY_DATA", Context.MODE_PRIVATE);
+    String result=share.getString("Social","");
+        rrr.setText(result);
+        */
 }
