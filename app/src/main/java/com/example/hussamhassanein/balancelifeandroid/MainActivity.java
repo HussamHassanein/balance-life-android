b4d3cdd4a549017ec34f585702a15409fe725475
kuku_eri@yahoo.com <Kewser@kuku-eriyahoocoms-MacBook-Pro.local>
2017-11-26 13:47:21 +0100

    Checkbox added to Review

--- app/src/main/java/com/example/hussamhassanein/balancelifeandroid/MainActivity.java
+++ app/src/main/java/com/example/hussamhassanein/balancelifeandroid/MainActivity.java
@@ -6,9 +6,12 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,7 +19,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        File f = getDatabasePath("Mydata.xml");
 
+        if (f != null)
+            Log.i("TAG", f.getAbsolutePath());
     }
 
 
